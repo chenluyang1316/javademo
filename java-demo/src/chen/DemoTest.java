a25bf73320ccd1b7d9c5ebf7c7a62ca13fcc814e
chenly <chenluyang1519@163.com>
2019-08-22 16:22:49 +0800

    修改提交demo1

--- java-demo/src/chen/DemoTest.java
+++ java-demo/src/chen/DemoTest.java
@@ -4,5 +4,6 @@ public class DemoTest {
     public static void main(String[] args) {
         System.out.println(System.currentTimeMillis());
         System.out.println("更新了");
+        System.out.println("demo1修改");
     }
 }
